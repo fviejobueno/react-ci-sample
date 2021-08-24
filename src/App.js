f22a04d03bd6876d1861cc6b69a5f0775a904590
Francisco Viejobueno <francisco.v@thoughtworks.com>
2021-08-24 12:04:12 +0200

    Factoria Workshop | Fernando and Paquito | includes form for player name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from "react";
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [playerName, setPlayerName] = useState("")
+
+
+    return (
+
+        <div className="Form">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Include Player Name.
+                </p>
+                <form>
+                    <label>
+                        Name:
+                        <input onChange={(e) => {setPlayerName(e.target.value)}} type="text" name="name"/>
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
+                <p>
+                    {playerName}
+                </p>
+            </header>
+
+        </div>
+
+    );
 }
 
 export default App;
