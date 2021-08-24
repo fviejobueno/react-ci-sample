4f92f2e7a29eba9f4aff2cee4387ff0ef28cb6bb
Francisco Viejobueno <francisco.v@thoughtworks.com>
2021-08-24 12:56:10 +0200

    Factoria Workshop | Fernando and Paquito | includes awesome button dependency to render an awesome button and cretes the state for the list of players

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React, { useState } from "react";
+import React, {useState} from "react";
 
+import {AwesomeButton} from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
 
 function App() {
 
     const [playerName, setPlayerName] = useState("")
-
+    const [players, setPlayers] = useState([])
 
     return (
 
@@ -15,18 +17,31 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
-                    Include Player Name.
+                    New Player name is <code>{playerName}</code>
                 </p>
                 <form>
                     <label>
                         Name:
-                        <input onChange={(e) => {setPlayerName(e.target.value)}} type="text" name="name"/>
+                        <input onChange={(e) => {
+                            setPlayerName(e.target.value)
+                        }} type="text" name="name"/>
                     </label>
-                    <input type="submit" value="Submit"/>
+                    <AwesomeButton
+                        type="primary"
+                        ripple={true}
+                        onPress={() => {
+                            const newList = players.concat({name: playerName, id: Math.random()})
+                            setPlayers(newList)
+                        }}
+                    >
+                        Save
+                    </AwesomeButton>
                 </form>
-                <p>
-                    {playerName}
-                </p>
+                <ul>
+                    {players.map((item) => (
+                        <li key={item.id}>{item.name}</li>
+                    ))}
+                </ul>
             </header>
 
         </div>
